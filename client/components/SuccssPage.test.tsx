f65f8dbc95c28f9424160c18605096a41696e157
ahmad anwar <eng.ahmed.anwar@gmail.com>
2023-04-14 11:32:16 +1200

    install jest-dom

--- client/components/SuccssPage.test.tsx
+++ client/components/SuccssPage.test.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 import SuccessPage from '../SuccessPage'
-
 // type for name prop
 type SuccessPageProps = {
   name: string
 }
 
 // test for SuccessPage component
-test('renders success page with correct props and checks status button functionality ', () => {
-  const name: string = 'John Smith'
+test('renders success page with correct props and checks status button functionality', () => {
+  const name = 'John Smith'
   const checkStatusMock = jest.fn()
   render(<SuccessPage name={name} checkSatus={checkStatusMock} />)
   const greetingElement = screen.getByText(`Hey ${name}`)
