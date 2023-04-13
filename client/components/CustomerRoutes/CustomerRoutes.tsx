b21e218764142249ca4e2f55906097a7e6740b3d
phattharanit-pewklieng <phattharanit.noei@gmail.com>
2023-04-14 09:38:14 +1200

    nav

--- client/components/CustomerRoutes/CustomerRoutes.tsx
+++ client/components/CustomerRoutes/CustomerRoutes.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../Home'
+import MenuItems from '../Menu/MenuItems'
+import Contact from '../Contact'
+
 
 function CustomerRoutes() {
   return (
-    <div>CustomerRoutes</div>
+    <>
+      <div className="lg:flex tex-center md:text-align">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/table/:tableNo/menu" element={<MenuItems />} />
+          <Route path="/table/:tableNo/aboutus" element={<Contact />} />
+        </Routes>
+      </div>
+    </>
   )
 }
 
--- client/components/HomeMenu.tsx
+++ client/components/HomeMenu.tsx
@@ -1,33 +1,70 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { useState } from 'react'
-import {useParams, NavLink, Route, Routes} from 'react-router-dom'
-import {GiHamburgerMenu} from 'react-icons/gi'
+import { useParams, Route, Routes } from 'react-router-dom'
 
- 
+// React-Icons Import
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { IoClose } from 'react-icons/io5'
 
 function HomeMenu() {
-const {id } = useParams()
+  const { id } = useParams()
 
-const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false)
 
-const toggleMenu = () => {
-  setOpen((prev)=> !prev)
-}
+  const toggleMenu = () => {
+    setOpen((prev) => !prev)
+  }
   return (
     <>
-     <nav className={open ? 'block'  : 'hidden' + 'w-full lg:flex justify-between items-center lg:items-center lg:w-auto'}>
-        <div className='lg:flex tex-center md:text-align'>
+      <Routes>
+        <Route>
+          <nav
+            className={
+              open
+                ? 'block'
+                : 'hidden' +
+                  'w-full lg:flex justify-between items-center lg:items-center lg:w-auto'
+            }
+          >
+            {/* <div className="lg:flex tex-center md:text-align">
           <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/table/:tableNo/menu' element={<Menultems/>}/>
-            <Route path='/table/:tableNo/aboutus' element={<Contact/>}/>
-
-            
+            <Route path="/" element={<Home />} />
+            <Route path="/table/:tableNo/menu" element={<Menultems />} />
+            <Route path="/table/:tableNo/aboutus" element={<Contact />} />
           </Routes>
+        </div> */}
 
-        </div>
-     </nav>
+            <div className="lg:flex text-center md:text-align">
+              <Link
+                to="/"
+                className="text-white hover:text-green block my-6 py-2 px-6"
+              >
+                Home
+              </Link>
+
+              <Link
+                to="/table/:tableNo/menu"
+                className="text-white hover:text-green block my-6 py-2 px-6"
+              >
+                Menu
+              </Link>
+
+              <Link
+                to="/table/:tableNo/contact"
+                className="text-white hover:text-green block my-6 py-2 px-6"
+              >
+                Contact 
+              </Link>
+            </div>
+          </nav>
+          <div
+            className="lg:hidden z-90 absolute top-4 right-6 text-white text-4xl"
+            onClick={toggleMenu}
+          >
+            {open ? <IoClose /> : <GiHamburgerMenu />}
+          </div>
+        </Route>
+      </Routes>
     </>
   )
 }
