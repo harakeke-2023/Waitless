c466cb662dc10b3bbac1c9ee8a35b594cf61fe57
Sean Stickland <sticklandsm@gmail.com>
2023-04-14 14:30:22 +1200

    Made more API stuff

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HomeMenu from './HomeMenu'
 import CategoriesNavBar from './Menu/CategoriesNavBar'
+import { Header } from './Header'
 
 export default function Home() {
   return (
     <div>
+      <Header isHome={true} />
       <HomeMenu />
       <CategoriesNavBar />
     </div>
