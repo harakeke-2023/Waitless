9dd74e129c48ce02de7a840e7656aa1c78dbb799
Caroline Stadnicki <carolinestadnicki.nz@gmail.com>
2023-04-14 16:19:18 +1200

    Add appetizers to menu page

--- client/components/Menu/Categories.tsx
+++ client/components/Menu/Categories.tsx
@@ -1,5 +1,18 @@
 import React from 'react'
+import { MenuItemMutation } from '../../../models/MenuItem'
 
-export default function Categories() {
-  return <></>
+interface Props{
+  category: MenuItemMutation[]
+}
+
+export default function Categories(props: Props) {
+return(
+  <>
+  <ul>
+  {props.category.map((item)=>{
+    return<li key={item.id}>{item.name}</li>
+  })}
+  </ul>
+  </>
+)
 }
