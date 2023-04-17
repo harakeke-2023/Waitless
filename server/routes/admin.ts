bcf75804c360e5a3996c22fcbfa0fe1e715c94c1
phattharanit-pewklieng <phattharanit.noei@gmail.com>
2023-04-17 12:02:25 +1200

    admid route test/ jest cover package

---
+++ server/routes/admin.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import * as db from '../db/customer_orders'
+
+const router = express.Router()
+
+//Get /api/v1/admin/orders
+router.get('/orders', async (req, res) => {
+  try {
+    const customerOrders = await db.getAllCustomerOrdersWithDetails()
+    console.log(customerOrders)
+
+    res.json({ customerOrders })
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message)
+      res.status(500).json({
+        error: {
+          title: 'Order is not success! Please try again',
+        },
+      })
+    }
+  }
+})
+
+export default router
