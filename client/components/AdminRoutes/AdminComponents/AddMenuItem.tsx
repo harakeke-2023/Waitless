9b2cef7416b74039c95b75c08a713082d4b4b3df
phattharanit-pewklieng <phattharanit.noei@gmail.com>
2023-04-18 16:21:25 +1200

    update

--- client/components/AdminRoutes/AdminComponents/AddMenuItem.tsx
+++ client/components/AdminRoutes/AdminComponents/AddMenuItem.tsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from 'react'
 import ItemForm from './ItemForm'
+import { MenuItemMutation } from '../../../../models/MenuItem'
+import { addMenuItem } from '../../../apis/menuItems'
 
 export default function AddMenuItem() {
+  const [addItem, setAddItem] = useState({
+    id: 0,
+    name: '',
+    description: '',
+    price: 0,
+    stock: 0,
+    image_url: '',
+    category_id: 0,
+  } as MenuItemMutation)
+
+  // useEffect(() => {
+  //   addMenuItem()
+  //     .then((item) => {
+  //       setAddItem(() => item)
+  //     })
+  //     .catch((error) => {
+  //       console.error(error)
+  //     })
+  // }, [])
+
+  // const newItem = (addItem) => {
+  //   setAddItem([...item, addItem])
+  // }
+
   return (
     <div>
-      <ItemForm />
+      <ItemForm item={addItem} />
     </div>
   )
 }
