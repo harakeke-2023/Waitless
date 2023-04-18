1dda07d7a23f9a75c0a8509b1645789f56542b27
Sean Kim <ksiw0427@gmail.com>
2023-04-19 11:00:11 +1200

    uppercase headings

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -49,7 +49,7 @@ export function Header() {
           >
             <div className="flex h-full items-center justify-center">
               <div className="px-6 text-center text-white md:px-12">
-                <h1 className="mb-6 md:text-5xl max-sm:text-3xl font-bold">
+                <h1 className="mb-6 uppercase md:text-5xl max-sm:text-3xl font-bold">
                   {headingName}
                 </h1>
                 <h3 className="mb-8 md:text-3xl max-sm:text-xl font-bold">
