6f5f6d18b24770dc8f476ed5a7b7a44630f9cb04
Sean Stickland <sticklandsm@gmail.com>
2023-04-19 16:35:11 +1200

    editting

--- client/components/FailedPage.tsx
+++ client/components/FailedPage.tsx
@@ -25,11 +25,11 @@ export interface Props {
   retry: () => void // Define the checkStatus prop
 }
 
-const FailedPage = ({ message, retry }: Props) => {
+const FailedPage = ({ retry }: Props) => {
   return (
     <div className="text-center mt-10">
       <h1 className="text-3xl font-bold mb-2">Order failed </h1>
-      <p className="text-lg text-gray-600 mb-4">{message}</p>
+      <p className="text-lg text-gray-600 mb-4"></p>
       <p className="text-lg text-gray-600 mb-4">
         Please contact customer support for assistance
       </p>
